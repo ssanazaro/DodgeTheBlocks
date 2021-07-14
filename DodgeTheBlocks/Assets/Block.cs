a7861414bcb870806acdaaa54c267e64b3655377
jackdaw5 <s.sanazaro91@yahoo.com>
2021-07-14 15:54:08 -0500

    Initial Upload

---
+++ DodgeTheBlocks/Assets/Block.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	// Update is called once per frame
+	void Start()
+	{
+		GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad / 20f;
+	}
+	
+	void Update () 
+	{
+		if (transform.position.y < -3f)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ DodgeTheBlocks/Assets/BlockSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+	public Transform[] spawnPoints;
+
+	public GameObject blockPrefab;
+
+	private float timeToSpawn = 2f;
+
+	public float timeBetweenWaves = 1f;
+	// Use this for initialization
+	void Update () 
+	{
+		if (Time.time >= timeToSpawn)
+		{
+			SpawnBlocks();
+			timeToSpawn = Time.time + timeBetweenWaves;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void SpawnBlocks ()
+	{
+		int randomIndex = Random.Range(0, spawnPoints.Length);
+
+		for (int i = 0; i < spawnPoints.Length; i++)
+		{
+			if (randomIndex != i)
+			{
+				Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+			}
+		}
+	}
+}
